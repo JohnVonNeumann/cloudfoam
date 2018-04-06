3024fe18bbe86a69f9c6d0df8feb26b5e8e546e9
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-06 21:03:23 +1000

    CREATE cloudfoam.py - add bad pseudocode for general layout of function

---
+++ cloudfoam.py
@@ -0,0 +1,8 @@
+# function ( file_to_copy, location_to_copy_to )
+#     set file_to_copy
+#     set location_to_copy_to
+#     generate actual paths from the input strings
+#     create copy of file
+#     set copy
+#     send copy to location
+#     exit
