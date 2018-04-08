19702fdc386c87624042eded4b48294fc8877b30
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-08 18:48:23 +1000

    UPDATE test_cli.py - pycodestyle fixes

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,4 +1,4 @@
-from os import path 
+from os import path
 from os import remove
 
 import click
@@ -6,12 +6,15 @@ from click.testing import CliRunner
 
 from cloudfoam import main
 
+
 def test_cli_copy():
     runner = CliRunner()
-    result = runner.invoke(main, ['AWS::EC2::Subnet.yaml', \
-                                  '/home/lw/code/open_source/cloudfoam/test.yaml'])
+    result = runner.invoke(main,
+                           ['AWS::EC2::Subnet.yaml',
+                            '/home/lw/code/open_source/cloudfoam/test.yaml'])
     assert path.exists('/home/lw/code/open_source/cloudfoam/test.yaml')
     remove('/home/lw/code/open_source/cloudfoam/test.yaml')
+
 
 if __name__ == "__main__":
     test_cli_copy()
