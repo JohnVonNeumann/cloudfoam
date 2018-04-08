ee1bf1b41f562ec166c66ee7c5d26186f726e747
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-08 18:44:39 +1000

    CREATE test dir - add cli_tool test to check everything works

---
+++ test/test_cli.py
@@ -0,0 +1,17 @@
+from os import path 
+from os import remove
+
+import click
+from click.testing import CliRunner
+
+from cloudfoam import main
+
+def test_cli_copy():
+    runner = CliRunner()
+    result = runner.invoke(main, ['AWS::EC2::Subnet.yaml', \
+                                  '/home/lw/code/open_source/cloudfoam/test.yaml'])
+    assert path.exists('/home/lw/code/open_source/cloudfoam/test.yaml')
+    remove('/home/lw/code/open_source/cloudfoam/test.yaml')
+
+if __name__ == "__main__":
+    test_cli_copy()
