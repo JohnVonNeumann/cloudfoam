ca6938f5b5f562deeeed039e78aab2a2b99008a2
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-08 17:22:35 +1000

    UPDATE cloudfoam.py - chlean up pycodestyle errors

--- cloudfoam.py
+++ cloudfoam.py
@@ -1,23 +1,15 @@
-# function ( file_to_copy, location_to_copy_to )
-#     set file_to_copy
-#     set location_to_copy_to
-#     generate actual paths from the input strings
-#     create copy of file
-#     set copy
-#     send copy to location
-#     exit
-
-from shutil import copyfile, copy
+from shutil import copy
 
 import click
+
 
 @click.command()
 @click.argument('template')
 @click.argument('dest')
-
 def main(template, dest):
     copy(template, dest)
     click.echo("Done")
 
+
 if __name__ == "__main__":
-    main() 
+    main()
